2522d1d72a554477d1ec08389f6470cc48b7e257
NoBoDy-Adk <164737895+NoBoDy-Adk@users.noreply.github.com>
2025-07-31 12:52:21 +0530

    30-07

---
+++ project/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-content">
+        <h2>Order food from here.</h2>
+       <p>Food provides essential nutrients for overall health and well-being</p>
+       <button>View menu</button> 
+      </div>
+    </div>
+  )
+}
+
+export default Header
